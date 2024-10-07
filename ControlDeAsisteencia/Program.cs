1acf48b5490c8d007f57f7575b162d686e0c81c7
Axel Gomez <axelvado55@gmail.com>
2024-10-06 22:30:32 -0600

    Ya en la clase principal "Program"
    Creamos varios estudiantes utilizando la clase Estudiante de Universidad

--- ControlDeAsisteencia/Program.cs
+++ ControlDeAsisteencia/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+using ControlDeAsisteencia; 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Universidad.Estudiante[] estudiantes = new Universidad.Estudiante[]
+        {
+            new Universidad.Estudiante("Carlos Pérez", 20, 15),
+            new Universidad.Estudiante("Ana García", 25, 22),
+            new Universidad.Estudiante("Luis Fernández", 30, 20),
+            new Universidad.Estudiante("Marta López", 20, 14)
+        };
