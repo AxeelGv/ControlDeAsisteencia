d0e37c950ea35f9016917b7116e23f82b2372b8d
Axel Gomez <axelvado55@gmail.com>
2024-10-06 22:13:59 -0600

    Se creo la clase Universidad con su clase anidada Estudiante, con sus propiedades.
    Tambien se implemento la clase interna Asistencia, en la cual se calcula si el estudiante cumple con el minimo y demas calculos.

---
+++ ControlDeAsisteencia/Universidad.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlDeAsisteencia
+{
+    public class Universidad
+    {
+        public class Estudiante
+        {
+            public string Nombre { get; set; }
+            public int TotalSesiones { get; set; }
+            public int SesionesAsistidas { get; set; }
+
+
+            public Estudiante(string nombre, int totalSesiones, int sesionesAsistidas)
+            {
+                Nombre = nombre;
+                TotalSesiones = totalSesiones;
+                SesionesAsistidas = sesionesAsistidas;
+            }
+
+
+            public class Asistencia
+            {
+
+                public static double CalcularPorcentaje(int totalSesiones, int sesionesAsistidas)
+                {
+                    if (totalSesiones == 0)
+                    {
+                        return 0;
+                    }
+                    return (double)sesionesAsistidas / totalSesiones * 100;
+                }
+
+
+                public static bool CumpleMinimo(int totalSesiones, int sesionesAsistidas)
+                {
+                    return CalcularPorcentaje(totalSesiones, sesionesAsistidas) >= 75;
+                }
+            }
+
+
+        }
+    }
+}
